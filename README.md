9226914186f039d442959f72692b793fd4e1f81b
Elliott Hilaire <elliott.hilaire@gmail.com>
2017-12-30 13:55:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# README.md
+
+A simple Rails template
+
+In Age of Empires 2 the first two minutes can make or break a game.
+But it's always the same tasks. Queue up villagers, gather sheep, build a house,
+chop wood, pick berries, build a house, lure the boar...
+
+This is a Rails app template to automate the start game.
+
+```bash
+rails new project --database=postgresql --template=https://raw.githubusercontent.com/elliotthilaire/rails-app-template/master/template.rb --skip-coffee
+```
---
+++ template.rb
@@ -0,0 +1,19 @@
+
+# Enable UUID extension
+generate :migration, "enable_uuid_extension"
+enable_uuid_extension_migration_path = Dir.glob("db/migrate/*_enable_uuid_extension.rb").last
+insert_into_file enable_uuid_extension_migration_path, :after => "  def change" do
+  "\n    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')"
+end
+
+# Generate User model
+generate :model, "user"
+gsub_file Dir.glob("db/migrate/*_create_users.rb").last,
+  "create_table :users do |t|", "create_table :users, id: :uuid do |t|"
+
+# Generate EmailAddress model
+generate :model, "email_address email_address:string:uniq user:uuid:references"
+
+# Create index controller
+generate :controller, "welcome index"
+route "root to: 'welcome#index'"
